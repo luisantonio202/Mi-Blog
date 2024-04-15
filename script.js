ac18b2527dccde2329b6f3e2c7917e50cdc5e586
luisantonio202 <162599487+luisantonio202@users.noreply.github.com>
2024-04-15 15:43:28 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Función que se ejecutará cuando se haga clic en el botón
+function mostrarAlerta() {
+  alert("Hola que tengas un buen dia!");
+}
+
+// Espera a que el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", function() {
+  // Obtener el botón por su ID
+  var boton = document.getElementById("miBoton");
+
+  // Agregar un event listener al botón para detectar el clic y ejecutar la función mostrarAlerta
+  boton.addEventListener("click", mostrarAlerta);
+});
