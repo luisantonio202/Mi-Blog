d764bd9b8052e4ffbf7f4d5b669e9af3a4967180
luisantonio202 <162599487+luisantonio202@users.noreply.github.com>
2024-08-18 21:29:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function mostrarMensaje() {
 
   // Agregar un event listener al botón para detectar el clic y ejecutar la función mostrarAlerta
   botonSaludo.addEventListener("click", mostrarMensaje);
+
+document.getElementById("boton-inicio").addEventListener("click", function() {
+    window.location.href = "https://luisantonio202.github.io/Mi-Blog/";
+});
+
+document.getElementById("boton-notas").addEventListener("click", function() {
+  window.location.href = "https://luisantonio202.github.io/Mi-Blog-2/";
+});
+
+document.getElementById("boton-proyectos").addEventListener("click", function() {
+  window.location.href = "https://luisantonio202.github.io/Mi-Blog-3/";
+});
